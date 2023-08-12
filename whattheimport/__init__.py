41e5700076d42bc2b0f7771f9a35e29286ced0c2
Stephan Majert <stephan.majert@gmail.com>
2023-08-12 11:43:59 +0200

    Fix bug: unreadable version number after installation

--- whattheimport/__init__.py
+++ whattheimport/__init__.py
@@ -1,5 +1,1 @@
-from pathlib import Path
-import tomllib
-
-with open(Path(__file__).parent.parent / "pyproject.toml", "rb") as proj_toml:
-    __version__ = tomllib.load(proj_toml)["project"]["version"]
+__version__ = "0.0.1"
